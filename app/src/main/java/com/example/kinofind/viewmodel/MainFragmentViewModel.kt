3dc96c48d5d37b71675a753cbc36cd3d549e5b97
ruslankd <ruslankd@mail.ru>
2021-05-17 22:37:13 +0300

    Lesson 6

--- app/src/main/java/com/example/kinofind/viewmodel/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/kinofind/viewmodel/MainFragmentViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.example.kinofind.model.AppState
 import com.example.kinofind.model.Repository
 import com.example.kinofind.model.RepositoryImpl
+import com.example.kinofind.model.entities.Film
 
 class MainFragmentViewModel(
         private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
@@ -14,6 +15,13 @@ class MainFragmentViewModel(
     fun getLiveData() = liveDataToObserve
 
     fun getData() = getTopRatedFilmsFromServer()
+
+    fun setData(appState: AppState) {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            liveDataToObserve.postValue(appState)
+        }.start()
+    }
 
     private fun getTopRatedFilmsFromServer() {
         liveDataToObserve.value = AppState.Loading
